a597e1630a6271e364d80aa27828799f7eba5f0e
Jainil <cricket02pro@gmail.com>
2026-07-05 14:24:07 +0530

    build fix

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -31,23 +31,33 @@ const categoriesInfo = [
     name: "Western",
     sections: [
       {
-        name: "Bottoms",
+        name: "Jeans",
         items: [
-          { name: "Jeans", image: "https://images.unsplash.com/photo-1541099649105-f69ad21f3246?q=80&w=600&auto=format" },
+          { name: "Jeans", image: "https://images.unsplash.com/photo-1541099649105-f69ad21f3246?q=80&w=600&auto=format" }
+        ]
+      },
+      {
+        name: "Lower",
+        items: [
           { name: "Lower", image: "https://images.unsplash.com/photo-1506629082955-520b69af7b0d?q=80&w=600&auto=format" }
         ]
       },
       {
-        name: "Tops",
+        name: "Night Wear",
         items: [
-          { name: "Top", image: "https://images.unsplash.com/photo-1551163943-3f6a855d1153?q=80&w=600&auto=format" },
-          { name: "Cortset", image: "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?q=80&w=600&auto=format" }
+          { name: "Night Wear", image: "https://images.unsplash.com/photo-1515372039744-b8f02a3ae446?q=80&w=600&auto=format" }
         ]
       },
       {
-        name: "Loungewear",
+        name: "T Shirt",
         items: [
-          { name: "Night Wear", image: "https://images.unsplash.com/photo-1515372039744-b8f02a3ae446?q=80&w=600&auto=format" }
+          { name: "t shirt", image: "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?q=80&w=600&auto=format" }
+        ]
+      },
+      {
+        name: "Top",
+        items: [
+          { name: "Top", image: "https://images.unsplash.com/photo-1551163943-3f6a855d1153?q=80&w=600&auto=format" }
         ]
       }
     ]
