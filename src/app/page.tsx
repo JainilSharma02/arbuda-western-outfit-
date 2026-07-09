438cff2f73cd77768cc69d5ff9b44622a66c5a1f
Jainil <cricket02pro@gmail.com>
2026-07-09 12:58:49 +0530

    chore: remove Curated Collections section from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Hero from "@/components/home/Hero";
-import CategoriesSection from "@/components/home/CategoriesSection";
 import FeaturedProducts from "@/components/home/FeaturedProducts";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <CategoriesSection />
       <FeaturedProducts />
     </>
   );
